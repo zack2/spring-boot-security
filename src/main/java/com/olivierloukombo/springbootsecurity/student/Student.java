1b6609f1410255b974ed4788606265b35e604ee1
Olivier Loukombo <olivierloukombo@gmail.com>
2022-05-17 15:53:32 +0100

    first commit

---
+++ src/main/java/com/olivierloukombo/springbootsecurity/student/Student.java
@@ -0,0 +1,9 @@
+package com.olivierloukombo.springbootsecurity.student;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private final Integer studentId;
+    private final String studentName;
+}
---
+++ src/main/java/com/olivierloukombo/springbootsecurity/student/StudentController.java
@@ -0,0 +1,28 @@
+package com.olivierloukombo.springbootsecurity.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private  final List<Student> STUDENT = Arrays.asList(
+            new Student(1, "Zack olivier"),
+            new Student(2, "John Doe"),
+            new Student(3, "Jane Doe")
+    );
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId){
+        return STUDENT.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student " + studentId + " doesn't "));
+    }
+}
