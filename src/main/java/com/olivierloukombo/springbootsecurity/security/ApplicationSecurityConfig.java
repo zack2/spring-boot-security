429ce5839ef5b105233b8db87b50fe3f982b733c
Olivier Loukombo <olivierloukombo@gmail.com>
2022-05-19 10:05:14 +0100

    add in memory user and encode password

--- src/main/java/com/olivierloukombo/springbootsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/olivierloukombo/springbootsecurity/security/ApplicationSecurityConfig.java
@@ -1,14 +1,26 @@
 package com.olivierloukombo.springbootsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired public ApplicationSecurityConfig(PasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -18,5 +30,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser =  User.builder()
+                .username("anasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")
+                .build();
+        return new InMemoryUserDetailsManager( annaSmithUser);
     }
 }
