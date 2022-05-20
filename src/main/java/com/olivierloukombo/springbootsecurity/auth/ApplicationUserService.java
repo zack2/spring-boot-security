77ef1d3dcb922bc03468d60c0a1822570f466256
Olivier Loukombo <olivierloukombo@gmail.com>
2022-05-20 12:12:05 +0100

    add user service

---
+++ src/main/java/com/olivierloukombo/springbootsecurity/auth/ApplicationUserService.java
@@ -0,0 +1,14 @@
+package com.olivierloukombo.springbootsecurity.auth;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicationUserService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
