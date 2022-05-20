1837df59f5d7d2313c5859c61863030b4c387b20
Olivier Loukombo <olivierloukombo@gmail.com>
2022-05-20 13:41:48 +0100

    add JWT validates credentials

---
+++ src/main/java/com/olivierloukombo/springbootsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -0,0 +1,35 @@
+package com.olivierloukombo.springbootsecurity.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            UsernameAndPasswordAuthenticationRequest authenticationRequest = new ObjectMapper().readValue(request.getInputStream(),
+                    UsernameAndPasswordAuthenticationRequest.class);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.username(),
+                    authenticationRequest.password());
+            return authenticationManager.authenticate(authentication);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/olivierloukombo/springbootsecurity/jwt/UsernameAndPasswordAuthenticationRequest.java
@@ -0,0 +1,4 @@
+package com.olivierloukombo.springbootsecurity.jwt;
+
+public record  UsernameAndPasswordAuthenticationRequest(String username, String password) {
+}
