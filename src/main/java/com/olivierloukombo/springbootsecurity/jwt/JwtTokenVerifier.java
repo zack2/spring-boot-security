0459bb575da31d832e48b6fa8d517249e5ac4b91
Olivier Loukombo <olivierloukombo@gmail.com>
2022-05-21 12:34:44 +0100

    add JWT token verifier filters

---
+++ src/main/java/com/olivierloukombo/springbootsecurity/jwt/JwtTokenVerifier.java
@@ -0,0 +1,58 @@
+package com.olivierloukombo.springbootsecurity.jwt;
+
+import com.google.common.base.Strings;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class JwtTokenVerifier extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+        if(Strings.isNullOrEmpty(authorizationHeader) || !authorizationHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+        }
+        String token = authorizationHeader.replace( "Bearer ", "");
+        try{
+
+            String secretKey = "securesecuresecuresecuresecuresecuresecuresecuresecuresecuresecuresecure";
+            Jws<Claims> claimsJws = Jwts.parserBuilder()
+                   // .parser()
+                    .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                    .build()
+                    .parseClaimsJws(token);
+                   // .parseClaimsJws(token);
+
+            Claims body = claimsJws.getBody();
+            String username = body.getSubject();
+            List<Map<String, String>> authorities = (List<Map<String, String>>) body.get("authorities");
+            Set<SimpleGrantedAuthority> simpleGrantedAuthoritySet = authorities
+                    .stream()
+                    .map( authorityMap -> new SimpleGrantedAuthority(authorityMap.get("authority")))
+                    .collect(Collectors.toSet());
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    username,
+                    null,
+                    simpleGrantedAuthoritySet
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }catch (JwtException e){
+            throw new IllegalStateException("Untrusted Token" + token);
+        }
+    }
+}
